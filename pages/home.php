9c8ba37b35d1de1467b0b71364be3e4343241e06
CJona <jonathan.cameron@student.rocvf.nl>
2021-12-04 16:31:14 +0100

    Home pagina opmaak

--- pages/home.php
+++ pages/home.php
@@ -2,13 +2,87 @@
 // zodat de code alleen vanuit index.php uitgevoerd mag worden
 if (!defined('START')) die;
 ?>
-<section class="section">
-    <div class="container">
-        <h1 class="title">
-            Hello World
-        </h1>
-        <p class="subtitle">
-            My first website with <strong>Bulma</strong>!
+<section class="hero is-danger">
+    <div class="hero-body">
+        <p class="title">
+            Go-Electric
         </p>
+        <p class="subtitle">
+            De leverancier voor al uw elektrische vervoermiddelen
+        </p>
+        <a class="button is-warning is-large">
+            <strong>Neem contact op</strong>
+        </a>
     </div>
 </section>
+<div class="container py-4">
+    <section class="hero is-small">
+        <div class="hero-body">
+            <p class="title">
+                Fietsen
+            </p>
+        </div>
+    </section>
+    <div class="tile is-ancestor">
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">One</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Two</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Three</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Four</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+    </div>
+</div>
+
+<div class="container py-4">
+    <section class="hero is-small">
+        <div class="hero-body">
+            <p class="title">
+                Fietsen
+            </p>
+        </div>
+    </section>
+    <div class="tile is-ancestor">
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">One</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Two</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Three</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Four</p>
+                <p class="subtitle">Subtitle</p>
+            </article>
+        </div>
+    </div>
+</div>
--- pages/parts/header.php
+++ pages/parts/header.php
@@ -10,3 +10,63 @@ if (!defined('START')) die;
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 </head>
 <body>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+        </a>
+
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+            <a href="/" class="navbar-item">
+                Home
+            </a>
+
+            <a href="/?page=contact" class="navbar-item">
+                Contact
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link">
+                    More
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item">
+                        About
+                    </a>
+                    <a class="navbar-item">
+                        Jobs
+                    </a>
+                    <a class="navbar-item">
+                        Contact
+                    </a>
+                    <hr class="navbar-divider">
+                    <a class="navbar-item">
+                        Report an issue
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item">
+                <div class="buttons">
+                    <a class="button is-danger">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
